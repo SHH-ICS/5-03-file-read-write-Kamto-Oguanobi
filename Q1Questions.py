4efdf8db5c3276bdb43195403b7b5d79b1667d24
Kamto Oguanobi <kamto.oguanobi@stu.ocsb.ca>
2024-12-06 10:22:24 -0500

    Update Q1Questions.py
    Message for Github... My solution.

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,2 +1,4 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
+# My Solution goes here.
+print("im correct")
