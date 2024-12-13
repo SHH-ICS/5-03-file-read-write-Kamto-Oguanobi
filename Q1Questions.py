2fa0d7270261570d6e63af7117513a1aae46564f
Kamto Oguanobi <kamto.oguanobi@stu.ocsb.ca>
2024-12-13 10:39:00 -0500

    1

--- Q1Questions.py
+++ Q1Questions.py
@@ -1,4 +1,5 @@
 # Create a program that will accept a multiple choice question, four answers, and the letter of correct answer. 
 # This will be six lines, and then store the questions in the file questions.txt.
 # My Solution goes here.
-print("im correct")
+
+filehandle = open("filename.txt", 'w')
